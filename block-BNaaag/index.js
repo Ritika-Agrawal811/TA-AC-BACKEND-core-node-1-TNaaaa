22eb8d897caddaa597ae05609a1b16dcc97dece8
Ritika Agrawal <ritikaagrawal339@gmail.com>
2023-05-01 17:54:04 +0530

    complete: Assingnment Buffer & fs Module

--- block-BNaaag/index.js
+++ block-BNaaag/index.js
@@ -1,1 +1,18 @@
-// Setup
+const fs = require("fs");
+
+// asynchronous
+fs.readFile("./content.md", (err, content) => {
+  console.log(content.toString());
+});
+
+// synchronous
+const data = fs.readFileSync("./content.md");
+
+console.log("synchronous");
+console.log(data.toString());
+
+let buff1 = Buffer.alloc(10);
+console.log(buff1);
+
+buff1.write("Welcome to Buffer");
+console.log(buff1.toString());
